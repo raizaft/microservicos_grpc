82e65337239f8983f1cf96696e3a6cee8acd4bf0
Raiza Tomazoni <117646187+raizaft@users.noreply.github.com>
2026-01-30 08:56:12 -0300

    parte4

--- order/go.mod
+++ order/go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 replace github.com/raizaft/microservicos_grpc_proto => ../../microservicos_grpc_proto
 
 require (
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	github.com/raizaft/microservicos_grpc_proto v0.0.0-00010101000000-000000000000
 	google.golang.org/grpc v1.78.0
 )
--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -2,11 +2,17 @@ package payment
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/raizaft/microservicos_grpc/order/internal/application/core/domain"
 	pb "github.com/raizaft/microservicos_grpc_proto/golang/payment"
+
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type Adapter struct {
@@ -14,18 +20,54 @@ type Adapter struct {
 }
 
 func NewAdapter(url string) (*Adapter, error) {
-	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	// ---------- RETRY CONFIG ----------
+	var opts []grpc.DialOption
+
+	opts = append(opts,
+		grpc.WithUnaryInterceptor(
+			grpc_retry.UnaryClientInterceptor(
+				grpc_retry.WithCodes(
+					codes.Unavailable,
+					codes.ResourceExhausted,
+				),
+				grpc_retry.WithMax(5),
+				grpc_retry.WithBackoff(grpc_retry.BackoffLinear(1*time.Second)),
+			),
+		),
+	)
+
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.Dial(url, opts...)
 	if err != nil {
 		return nil, err
 	}
-	return &Adapter{client: pb.NewPaymentClient(conn)}, nil
+
+	return &Adapter{
+		client: pb.NewPaymentClient(conn),
+	}, nil
 }
 
 func (a *Adapter) Charge(order *domain.Order) error {
-	_, err := a.client.Create(context.Background(), &pb.CreatePaymentRequest{
+
+	// ---------- TIMEOUT ----------
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	_, err := a.client.Create(ctx, &pb.CreatePaymentRequest{
 		UserId:     order.CustomerID,
 		OrderId:    order.ID,
 		TotalPrice: order.TotalPrice(),
 	})
-	return err
+
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.DeadlineExceeded {
+			log.Println("⏰ Timeout ao chamar Payment")
+		}
+		return err
+	}
+
+	return nil
 }
